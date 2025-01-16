183d2242f05000a376842ca960d7ddb56adb9e83
cyanog3n <falseacckkys@gmail.com>
2025-01-16 21:31:01 +0800

    merge 2d48d639-785b4a54

--- src/main/java/com/cyanogen/experienceobelisk/block_entities/bibliophage/InfectedArchiversBookshelfEntity.java
+++ src/main/java/com/cyanogen/experienceobelisk/block_entities/bibliophage/InfectedArchiversBookshelfEntity.java
@@ -1,5 +1,6 @@
 package com.cyanogen.experienceobelisk.block_entities.bibliophage;
 
+import com.cyanogen.experienceobelisk.config.Config;
 import com.cyanogen.experienceobelisk.registries.RegisterBlockEntities;
 import net.minecraft.core.BlockPos;
 import net.minecraft.world.level.block.state.BlockState;
@@ -13,6 +14,14 @@ public class InfectedArchiversBookshelfEntity extends AbstractInfectedBookshelfE
         super.spawnDelayMax = 220;
         super.orbValue = 6;
         super.spawns = 200;
+    }
+
+    @Override
+    public void onLoad() {
+        super.spawnDelayMin = Config.COMMON.archiversSpawnDelayMin.get();
+        super.spawnDelayMax = Config.COMMON.archiversSpawnDelayMax.get();
+        super.orbValue = Config.COMMON.archiversOrbValue.get();
+        super.spawns = Config.COMMON.archiversSpawns.get();
     }
 
 }
